cbc31a60162fff2181e8d7aa14a6ed6c502c532f
Sherlock Holo <sherlockya@gmail.com>
2018-06-10 09:10:21 +0800

    Add config

---
+++ config/config.go
@@ -0,0 +1,154 @@
+package config
+
+import (
+    "github.com/Unknwon/goconfig"
+    "errors"
+    "io/ioutil"
+    "strconv"
+)
+
+type Client struct {
+    SocksAddr string
+    SocksPort int
+
+    RemoteAddr string
+    RemotePort int
+
+    CA []byte
+
+    CrtFile string
+    KeyFile string
+}
+
+type Server struct {
+    BindAddr string
+    BindPort int
+
+    CA []byte
+
+    CrtFile string
+    KeyFile string
+}
+
+func ReadClient(cfgFile string) (Client, error) {
+    cfg, err := goconfig.LoadConfigFile(cfgFile)
+    if err != nil {
+        return Client{}, err
+    }
+
+    section, err := cfg.GetSection("client")
+    if err != nil {
+        return Client{}, err
+    }
+
+    client := Client{}
+
+    if remoteAddr, ok := section["remote_addr"]; ok {
+        client.RemoteAddr = remoteAddr
+    } else {
+        return Client{}, errors.New("need remote_addr")
+    }
+
+    if remotePort, ok := section["remote_port"]; ok {
+        client.RemotePort, err = strconv.Atoi(remotePort)
+        if err != nil {
+            return Client{}, err
+        }
+    } else {
+        return Client{}, errors.New("need remote_port")
+    }
+
+    if socksAddr, ok := section["socks_addr"]; ok {
+        client.SocksAddr = socksAddr
+    } else {
+        return Client{}, errors.New("need socks_addr")
+    }
+
+    if socksPort, ok := section["socks_port"]; ok {
+        client.SocksPort, err = strconv.Atoi(socksPort)
+        if err != nil {
+            return Client{}, err
+        }
+    } else {
+        return Client{}, errors.New("need socks_port")
+    }
+
+    if caFile, ok := section["ca_crt"]; ok {
+        ca, err := ioutil.ReadFile(caFile)
+        if err != nil {
+            return Client{}, err
+        }
+
+        client.CA = ca
+    } else {
+        return Client{}, errors.New("need ca_crt")
+    }
+
+    if crtFile, ok := section["crt"]; ok {
+        client.CrtFile = crtFile
+    } else {
+        return Client{}, errors.New("need crt")
+    }
+
+    if keyFile, ok := section["key"]; ok {
+        client.KeyFile = keyFile
+    } else {
+        return Client{}, errors.New("need key")
+    }
+
+    return client, nil
+}
+
+func ReadServer(cfgFile string) (Server, error) {
+    cfg, err := goconfig.LoadConfigFile(cfgFile)
+    if err != nil {
+        return Server{}, err
+    }
+
+    section, err := cfg.GetSection("server")
+    if err != nil {
+        return Server{}, err
+    }
+
+    server := Server{}
+
+    if bindAddr, ok := section["bind_addr"]; ok {
+        server.BindAddr = bindAddr
+    } else {
+        return Server{}, errors.New("need bind_addr")
+    }
+
+    if bindPort, ok := section["bind_port"]; ok {
+        server.BindPort, err = strconv.Atoi(bindPort)
+        if err != nil {
+            return Server{}, err
+        }
+    } else {
+        return Server{}, errors.New("need bind_port")
+    }
+
+    if caFile, ok := section["ca_crt"]; ok {
+        ca, err := ioutil.ReadFile(caFile)
+        if err != nil {
+            return Server{}, err
+        }
+
+        server.CA = ca
+    } else {
+        return Server{}, errors.New("need ca_crt")
+    }
+
+    if crtFile, ok := section["crt"]; ok {
+        server.CrtFile = crtFile
+    } else {
+        return Server{}, errors.New("need crt")
+    }
+
+    if keyFile, ok := section["key"]; ok {
+        server.KeyFile = keyFile
+    } else {
+        return Server{}, errors.New("need key")
+    }
+
+    return server, nil
+}
