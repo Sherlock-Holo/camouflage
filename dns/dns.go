a6cfc85ffd29321c5674592e7f729b42422eb398
Sherlock Holo <sherlockya@gmail.com>
2018-06-12 09:42:54 +0800

    Rewrite Query, add timeout
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

--- dns/dns.go
+++ dns/dns.go
@@ -1,114 +1,128 @@
 package dns
 
 import (
-    "errors"
-    "net"
-    "sync"
+	"context"
+	"errors"
+	"net"
+	"sync"
+	"time"
 
-    "github.com/miekg/dns"
+	"github.com/miekg/dns"
 )
 
 type Resolver struct {
-    Server string
+	Server string
 
-    client dns.Client
+	client dns.Client
 }
 
 type Result struct {
-    AIP    []net.IP
-    AAAAIP []net.IP
+	AIP    []net.IP
+	AAAAIP []net.IP
 
-    aLock    sync.Mutex
-    aaaaLock sync.Mutex
+	aLock    sync.Mutex
+	aaaaLock sync.Mutex
 }
 
 var DefaultResolver = Resolver{
-    Server: "127.0.0.1:53",
-    client: dns.Client{
-        // Timeout: 2 * time.Second,
-    },
+	Server: "127.0.0.1:53",
+	client: dns.Client{
+		Timeout: 2 * time.Second,
+	},
 }
 
-func (r *Resolver) Query(host string, ipv6 bool) (Result, error) {
-    // TODO: edns client subnet
+func (r *Resolver) qQery(host string, ipv6 bool, ctx context.Context) (Result, error) {
+	// TODO: edns client subnet
 
-    var msgs []*dns.Msg
+	var msgs []*dns.Msg
 
-    msg := new(dns.Msg)
-    msg.Id = dns.Id()
-    msg.RecursionDesired = true
-    msg.Question = []dns.Question{
-        {
-            Name:   dns.Fqdn(host),
-            Qclass: dns.ClassINET,
-            Qtype:  dns.TypeA,
-        },
-    }
+	msg := new(dns.Msg)
+	msg.Id = dns.Id()
+	msg.RecursionDesired = true
+	msg.Question = []dns.Question{
+		{
+			Name:   dns.Fqdn(host),
+			Qclass: dns.ClassINET,
+			Qtype:  dns.TypeA,
+		},
+	}
 
-    msgs = append(msgs, msg)
+	msgs = append(msgs, msg)
 
-    if ipv6 {
-        v6Msg := new(dns.Msg)
-        v6Msg.Id = dns.Id()
-        v6Msg.RecursionDesired = true
-        v6Msg.Question = []dns.Question{
-            {
-                Name:   dns.Fqdn(host),
-                Qclass: dns.ClassINET,
-                Qtype:  dns.TypeAAAA,
-            },
-        }
+	if ipv6 {
+		v6Msg := new(dns.Msg)
+		v6Msg.Id = dns.Id()
+		v6Msg.RecursionDesired = true
+		v6Msg.Question = []dns.Question{
+			{
+				Name:   dns.Fqdn(host),
+				Qclass: dns.ClassINET,
+				Qtype:  dns.TypeAAAA,
+			},
+		}
 
-        msgs = append(msgs, v6Msg)
-    }
+		msgs = append(msgs, v6Msg)
+	}
 
-    var (
-        result Result
-        group  = sync.WaitGroup{}
-    )
+	var (
+		result Result
+		group  = sync.WaitGroup{}
+	)
 
-    for _, msg := range msgs {
-        group.Add(1)
-        go func(msg *dns.Msg) {
-            defer group.Done()
+	for _, msg := range msgs {
+		group.Add(1)
+		go func(msg *dns.Msg) {
+			defer group.Done()
 
-            rMsg, _, err := r.client.Exchange(msg, r.Server)
-            if err != nil {
-                return
-            }
+			rMsg, _, err := r.client.ExchangeContext(ctx, msg, r.Server)
+			if err != nil {
+				return
+			}
 
-            for _, an := range rMsg.Answer {
-                switch an := an.(type) {
-                case *dns.A:
-                    result.aLock.Lock()
-                    result.AIP = append(result.AIP, an.A)
-                    result.aLock.Unlock()
+			for _, an := range rMsg.Answer {
+				switch an := an.(type) {
+				case *dns.A:
+					result.aLock.Lock()
+					result.AIP = append(result.AIP, an.A)
+					result.aLock.Unlock()
 
-                case *dns.AAAA:
-                    result.aaaaLock.Lock()
-                    result.AAAAIP = append(result.AAAAIP, an.AAAA)
-                    result.aaaaLock.Unlock()
+				case *dns.AAAA:
+					result.aaaaLock.Lock()
+					result.AAAAIP = append(result.AAAAIP, an.AAAA)
+					result.aaaaLock.Unlock()
 
-                case *dns.CNAME:
-                    cnameResult, err := r.Query(an.Target, ipv6)
-                    if err == nil {
-                        result.aLock.Lock()
-                        result.AIP = append(result.AIP, cnameResult.AIP...)
-                        result.aLock.Unlock()
+				case *dns.CNAME:
+					cnameResult, err := r.qQery(an.Target, ipv6, ctx)
+					if err == nil {
+						result.aLock.Lock()
+						result.AIP = append(result.AIP, cnameResult.AIP...)
+						result.aLock.Unlock()
 
-                        result.aaaaLock.Lock()
-                        result.AAAAIP = append(result.AAAAIP, cnameResult.AAAAIP...)
-                        result.aaaaLock.Unlock()
-                    }
-                }
-            }
-        }(msg)
-    }
-    group.Wait()
+						result.aaaaLock.Lock()
+						result.AAAAIP = append(result.AAAAIP, cnameResult.AAAAIP...)
+						result.aaaaLock.Unlock()
+					}
+				}
+			}
+		}(msg)
+	}
+	group.Wait()
 
-    if len(result.AIP) == 0 && len(result.AAAAIP) == 0 {
-        return Result{}, errors.New("resolve failed")
-    }
+	if len(result.AIP) == 0 && len(result.AAAAIP) == 0 {
+		return Result{}, errors.New("resolve failed")
+	}
 
-    return result, nil
+	return result, nil
+}
+
+func (r *Resolver) Query(host string, ipv6 bool, timeout time.Duration) (Result, error) {
+	var ctx context.Context
+
+	if timeout > 0 {
+		ctx, _ = context.WithTimeout(context.Background(), timeout)
+	} else {
+		ctx = context.Background()
+	}
+
+	return r.qQery(host, ipv6, ctx)
 }
--- dns/dns_test.go
+++ dns/dns_test.go
@@ -1,17 +1,17 @@
 package dns
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestResolver_Query(t *testing.T) {
-    resolver := DefaultResolver
-    resolver.Server = "114.114.114.114:53"
+	resolver := DefaultResolver
+	resolver.Server = "1.1.1.1:53"
 
-    if result, err := resolver.Query("www.baidu.com", true); err != nil {
-        t.Error(err)
-    } else {
-        fmt.Println(result)
-    }
+	if result, err := resolver.Query("www.baidu.com", true, -1); err != nil {
+		t.Error(err)
+	} else {
+		fmt.Println(result)
+	}
 }
