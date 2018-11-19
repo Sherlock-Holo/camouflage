910ce0e10a9e1b098124399df33b16a4c7cb1b44
Sherlock Holo <sherlockya@gmail.com>
2018-11-19 19:50:41 +0800

    Rewrite main server part
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Sherlock-Holo/camouflage/client"
 	configClient "github.com/Sherlock-Holo/camouflage/config/client"
@@ -45,10 +49,23 @@ func main() {
 			log.Fatal(err)
 		}
 
-		s, err := server.New(cfg)
-		if err != nil {
-			log.Fatal(err)
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt)
+
+		servers := server.New(cfg)
+		httpServers := make([]*http.Server, 0, len(servers))
+
+		for _, s := range servers {
+			httpServers = append(httpServers, s.Run())
 		}
-		s.Run()
+
+		<-signalChan
+		for _, httpServer := range httpServers {
+			timeout, _ := context.WithTimeout(context.Background(), time.Second)
+			if err := httpServer.Shutdown(timeout); err != nil {
+				log.Println(err)
+				httpServer.Close()
+			}
+		}
 	}
 }
