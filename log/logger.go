698931e5a00aa432d7487c6fc989125e3f51df3a
Sherlock Holo <sherlockya@gmail.com>
2019-03-13 14:42:23 +0800

    Rewrite log module
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

--- log/logger.go
+++ log/logger.go
@@ -2,70 +2,89 @@ package log
 
 import (
 	"log"
-	"os"
+	"sync"
 )
 
-var (
-	debugLogger   log.Logger
-	infoLogger    log.Logger
-	warningLogger log.Logger
-	errorLogger   log.Logger
-	fatalLogger   log.Logger
+const (
+	debugPrefix = "[DEBUG]:"
+	infoPrefix  = "[INFO]:"
+	warnPrefix  = "[WARN]:"
+	errorPrefix = "[ERROR]:"
+	fatalPrefix = "[FATAL]:"
 )
 
-func init() {
-	debugLogger.SetOutput(os.Stderr)
-	debugLogger.SetPrefix("[DEBUG]: ")
-
-	infoLogger.SetOutput(os.Stderr)
-	infoLogger.SetPrefix("[INFO]: ")
-
-	warningLogger.SetOutput(os.Stderr)
-	warningLogger.SetPrefix("[WARN]: ")
-
-	errorLogger.SetOutput(os.Stderr)
-	errorLogger.SetPrefix("[ERROR]: ")
-
-	fatalLogger.SetOutput(os.Stderr)
-	fatalLogger.SetPrefix("[FATAL]: ")
+var pool = sync.Pool{
+	New: func() interface{} {
+		return make([]interface{}, 0, 2)
+	},
 }
 
 func Debug(v ...interface{}) {
-	debugLogger.Println(v)
+	buf := pool.Get().([]interface{})
+	buf = append(buf, debugPrefix)
+	buf = append(buf, v...)
+
+	log.Println(buf...)
+
+	pool.Put(buf[:0])
 }
 
 func Debugf(f string, v ...interface{}) {
-	debugLogger.Printf(f, v)
+	log.Printf(debugPrefix+" "+f, v...)
 }
 
 func Info(v ...interface{}) {
-	infoLogger.Println(v)
+	buf := pool.Get().([]interface{})
+	buf = append(buf, infoPrefix)
+	buf = append(buf, v...)
+
+	log.Println(buf...)
+
+	pool.Put(buf[:0])
 }
 
 func Infof(f string, v ...interface{}) {
-	infoLogger.Printf(f, v)
+	log.Printf(debugPrefix+" "+f, v...)
 }
 
 func Warn(v ...interface{}) {
-	warningLogger.Println(v)
+	buf := pool.Get().([]interface{})
+	buf = append(buf, warnPrefix)
+	buf = append(buf, v...)
+
+	log.Println(buf...)
+
+	pool.Put(buf[:0])
 }
 
 func Warnf(f string, v ...interface{}) {
-	warningLogger.Printf(f, v)
+	log.Printf(warnPrefix+" "+f, v...)
 }
 
 func Error(v ...interface{}) {
-	errorLogger.Println(v)
+	buf := pool.Get().([]interface{})
+	buf = append(buf, errorPrefix)
+	buf = append(buf, v...)
+
+	log.Println(buf...)
+
+	pool.Put(buf[:0])
 }
 
 func Errorf(f string, v ...interface{}) {
-	errorLogger.Printf(f, v)
+	log.Printf(errorPrefix+" "+f, v...)
 }
 
 func Fatal(v ...interface{}) {
-	fatalLogger.Fatal(v)
+	buf := pool.Get().([]interface{})
+	buf = append(buf, fatalPrefix)
+	buf = append(buf, v...)
+
+	log.Fatalln(buf...)
+
+	pool.Put(buf[:0])
 }
 
 func Fatalf(f string, v ...interface{}) {
-	fatalLogger.Fatalf(f, v)
+	log.Fatalf(fatalPrefix+" "+f, v)
 }
